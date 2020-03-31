8fabf275c367851ca3187235fcf7d439701f6af8
asramya <51397506+asramya@users.noreply.github.com>
2020-03-31 11:36:26 +0530

    Add files via upload
    Home Work Week1 Day1

---
+++ Android.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Android {
+
+//	Create a class (Android)
+//	Create 5 variables (long, Boolean, String, int , float)
+//	Call them from main method and Print the value
+	
+	long price = 25000;
+	boolean camera = true;
+	String modelname = "vivo y96";
+	int screensize = 7;
+	float pixcell= 12.5f;
+	
+	public static void main(String[] args)
+	{
+	Android os = new Android();
+	System.out.println(os.price);
+	System.out.println(os.camera);
+	System.out.println(os.modelname);
+	System.out.println(os.screensize);
+	System.out.println(os.pixcell);
+	}
+
+	
+	
+	
+	
+}
